0b41fde824c852eca1ab5462dcc08c4149ec0d8b
ObstinateCode <2855017988@qq.com>
2021-07-08 18:18:07 +0800

    尝试第三次增改

--- gitDemo/src/com/myc/TestMain.java
+++ gitDemo/src/com/myc/TestMain.java
@@ -3,6 +3,7 @@ package com.myc;
 public class TestMain {
     public static void main(String[] args) {
         //System.out.println("我又改了。。。。");
+        //git config --global http.sslVerify "true"
     }
 
 }
