5039623118d00c4b67ea7292371a2063d33ea493
ObstinateCode <2855017988@qq.com>
2021-07-10 15:11:55 +0800

    test分支修改 测试冲突

--- gitDemo/src/com/myc/TestMain.java
+++ gitDemo/src/com/myc/TestMain.java
@@ -4,6 +4,8 @@ public class TestMain {
     public static void main(String[] args) {
         //System.out.println("我又改了。。。。");
         //git config --global http.sslVerify "true"
+
+        System.out.println("test branch 测试冲突");
     }
 
 }
